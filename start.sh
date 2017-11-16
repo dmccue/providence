c3022beabec74c6739e27ec5d38650036116a252
dmccue <dmccue@gmail.com>
2017-11-16 13:56:16 +0000

    update to start

--- start.sh
+++ start.sh
@@ -6,6 +6,7 @@ sudo airmon-ng start wlan0
 
 git reset --hard HEAD && git pull
 
-pgrep providence-monitor.sh 2>&1 >/dev/null || \
-  ./providence-monitor.sh 2>&1 | tee -a data/providence-monitor.log
+pgrep providence-monitor.sh 2>&1 >/dev/null \
+  && pkill providence-monitor.sh
+./providence-monitor.sh 2>&1 > data/providence-monitor.log
 sudo python -u probe.py -i wlan0mon | tee -a data/probe.csv
