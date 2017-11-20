ffa37ed84b5e2814269f52fa1fba26b37a4b7b34
David M <dmccue@gm[...].com>
2017-11-20 16:56:03 +0000

    updates

--- start.sh
+++ start.sh
@@ -5,7 +5,7 @@ export PYTHONUNBUFFERED=1
 
 SLACK_CMD='echo -e "$(date '+%Y\/%m\/%d_%H:%M:%S'):\n$(cat -)" | tee /dev/tty | slacker -c announce -n wifi-bot'
 
-git reset --hard HEAD && git pull
+#git reset --hard HEAD && git pull
 
 
 # Setup wifi monitor mode
@@ -15,15 +15,15 @@ sudo airmon-ng start wlan0
 
 
 # Start packet monitor
-while pgrep probe.py 2>&1 >/dev/null; do
-  pkill probe.py 2>&1 >/dev/null
+while pgrep -f python.*probe.py 2>&1 >/dev/null; do
+  sudo pkill -f python.*probe.py 2>&1 >/dev/null
   sleep 2
 done
 sudo python -u probe.py -i wlan0mon 2>&1 >> data/probe.csv &
 
 
 echo "Started watching files" | eval $SLACK_CMD
-trap '{ echo "Stopped watching files" | eval $SLACK_CMD ; exit 1; }' INT
+trap '{ sudo pkill -f python.*probe.py; echo "Stopped watching files" | eval $SLACK_CMD ; exit 1; }' INT
 
 # Start file listener
 inotifywait -m data |
